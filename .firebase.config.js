4b3a93c4d89f6a1bfb10dd90f78832759d566221
rjspyk5 <rjspyk5@gmail.com>
2024-07-16 18:57:53 +0600

    unknown

--- .firebase.config.js
+++ .firebase.config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,16 +1,23 @@
 import { createContext, useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../.firebase.config";
 
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [loading, setloading] = useState(false);
 
-  const signIn = (email, pass) => {
+  const signUp = (email, pass) => {
+    setloading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
+
+  const logIn = (email, pass) => {
     setloading(true);
     return signInWithEmailAndPassword(auth, email, pass);
   };
-
   const providerValue = { test: "test" };
   return (
     <AuthContext.Provider value={providerValue}>
