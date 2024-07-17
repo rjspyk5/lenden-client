4dc27a151dc7e6ecdc4e3f01d1013f517275ff0f
rjspyk5 <rjspyk5@gmail.com>
2024-07-17 19:27:05 +0600

    remove firebase config file and uninstall firebase

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,22 +1,9 @@
 import { createContext, useState } from "react";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
-import { auth } from "../../.firebase.config";
+
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [loading, setloading] = useState(false);
 
-  const signUp = (email, pass) => {
-    setloading(true);
-    return createUserWithEmailAndPassword(auth, email, pass);
-  };
-
-  const logIn = (email, pass) => {
-    setloading(true);
-    return signInWithEmailAndPassword(auth, email, pass);
-  };
   const providerValue = { test: "test" };
   return (
     <AuthContext.Provider value={providerValue}>
