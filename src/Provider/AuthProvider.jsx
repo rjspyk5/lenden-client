bdc9b6f049bc8ba6af8a238c45e40bb1ccbe35ed
rjspyk5 <rjspyk5@gmail.com>
2024-07-25 12:45:41 +0600

    basic stucture create for make login,reg fuctionality with database

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,10 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
-  const [loading, setloading] = useState(false);
+  const [loading, setloading] = useState(true);
+  const [user, setuser] = useState(null);
 
-  const providerValue = { test: "test" };
+  const login = (number, pass) => {
+    console.log("login");
+  };
+
+  const registration = (name, email, number, pass) => {
+    console.log("registration");
+  };
+
+  const logout = () => {
+    console.log("logout");
+  };
+
+  const { isPending, data, refetch } = useQuery({
+    queryKey: [login, logout, registration],
+    queryFn: async () => {
+      const info = localStorage.getItem("lenden_user_info") || null;
+      setuser(info);
+      setloading(false);
+      return info;
+    },
+  });
+
+  const providerValue = { loading, user };
   return (
     <AuthContext.Provider value={providerValue}>
       {children}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { AuthProvider } from "./Provider/AuthProvider.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Routing } from "./Routing/Routing.jsx";
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={Routing} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={Routing} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
