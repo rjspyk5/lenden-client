1c481eba7b6825d5696a2c8e1a833f7e173099bb
rjspyk5 <rjspyk5@gmail.com>
2024-07-29 14:47:57 +0600

    setup:auth setup for client site

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { createContext, useState } from "react";
+import { useAxiosPublic } from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [loading, setloading] = useState(true);
   const [user, setuser] = useState(null);
+  const axiosPublic = useAxiosPublic();
 
   const test = {
     name: "rakib",
@@ -20,6 +22,15 @@ export const AuthProvider = ({ children }) => {
       const info = localStorage.getItem("lenden_user") || null;
       setuser(info);
       setloading(false);
+      if (user) {
+        axiosPublic
+          .post("/jwt", { email: user?.email })
+          .then((res) => console.log(res.data));
+      } else {
+        axiosPublic
+          .post("/logout", { email: user?.email })
+          .then((res) => console.log(res.data));
+      }
       return info;
     },
   });
