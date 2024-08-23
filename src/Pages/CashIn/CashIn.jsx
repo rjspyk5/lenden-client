fb4e3eb18225c230c1464c897e03699bda3f4f9c
rjspyk5 <rjspyk5@gmail.com>
2024-08-23 23:21:10 +0600

    sendor cahsout component add in cashin component

--- src/Pages/CashIn/CashIn.jsx
+++ src/Pages/CashIn/CashIn.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { SendOrCashout } from "./../../Components/SendOrCashout/SendOrCashout";
 
 export const CashIn = () => {
-  return <div>CashIn</div>;
+  return (
+    <div>
+      <SendOrCashout methodparam="cash_in" />
+    </div>
+  );
 };
