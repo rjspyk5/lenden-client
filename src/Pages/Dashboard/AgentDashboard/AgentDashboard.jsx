010aabfdb0aaa09d3d748320fb118de2f95c2502
rjspyk5 <rjspyk5@gmail.com>
2024-08-25 19:06:49 +0600

    dashboard desgin start

--- src/Pages/Dashboard/AgentDashboard/AgentDashboard.jsx
+++ src/Pages/Dashboard/AgentDashboard/AgentDashboard.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const AgentDashboard = () => {
-  return <div>AgentDashboard</div>;
+  return (
+    <div className="flex">
+      <div className="w-[20%]">
+        <ul>
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+          <li>test4</li>
+        </ul>
+      </div>
+      <div className="ml-[20%]"></div>
+    </div>
+  );
 };
