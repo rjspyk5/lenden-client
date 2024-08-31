d2483aaa64eed5cdde24d93675bd996bd4e7f048
rjspyk5 <rjspyk5@gmail.com>
2024-08-31 14:12:29 +0600

    desgin:add money and withdraw money page

--- src/Components/AgentAddOrWithdrawMoney/AgentAddOrWithdrawMoney.jsx
+++ src/Components/AgentAddOrWithdrawMoney/AgentAddOrWithdrawMoney.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
 
 export const AgentAddOrWithdrawMoney = () => {
-  return <div>AgentAddOrWithdrawMoney</div>;
+  return (
+    <div>
+      <FormControl sx={{ m: 1 }}>
+        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-amount"
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          label="Amount"
+        />
+      </FormControl>
+      <Button variant="contained" endIcon={"send"}>
+        Send
+      </Button>
+    </div>
+  );
 };
