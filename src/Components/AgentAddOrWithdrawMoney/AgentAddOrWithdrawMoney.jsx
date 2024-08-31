406fcb62218a7ae04423094c1b5f11ea0836200c
rjspyk5 <rjspyk5@gmail.com>
2024-08-31 17:12:00 +0600

    desgin:agent dashboard section desgin

--- src/Components/AgentAddOrWithdrawMoney/AgentAddOrWithdrawMoney.jsx
+++ src/Components/AgentAddOrWithdrawMoney/AgentAddOrWithdrawMoney.jsx
@@ -5,21 +5,43 @@ import {
   InputLabel,
   OutlinedInput,
 } from "@mui/material";
-
+import { useForm } from "react-hook-form";
 export const AgentAddOrWithdrawMoney = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
-      <FormControl sx={{ m: 1 }}>
-        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-amount"
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
-          label="Amount"
-        />
-      </FormControl>
-      <Button variant="contained" endIcon={"send"}>
-        Send
-      </Button>
+      <form
+        className="flex flex-col justify-center min-h-96 items-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormControl sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            className="mb-5"
+            placeholder="Minimum value is 1000"
+            {...register("amount", { required: true, min: 1000 })}
+            id="outlined-adornment-amount"
+            startAdornment={<InputAdornment position="start">৳</InputAdornment>}
+            label="Amount"
+          />
+
+          {errors?.amount?.type === "required" && (
+            <span className="text-red-500">This field is required</span>
+          )}
+          {errors?.amount?.type === "min" && (
+            <span className="text-red-500">Give more that 1000 tk</span>
+          )}
+          <Button type="submit" variant="contained" endIcon={"send"}>
+            Send
+          </Button>
+        </FormControl>
+      </form>
     </div>
   );
 };
--- src/Pages/Dashboard/AgentDashboard/AgentTransictionHistory.jsx
+++ src/Pages/Dashboard/AgentDashboard/AgentTransictionHistory.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { SectionHeader } from "../../../Components/SectionHeader/SectionHeader";
+import CustomTable from "../../../Components/Table/CustomTable";
 
 export const AgentTransictionHistory = () => {
-  return <div>AgentTransictionHistory</div>;
+  return (
+    <div className="m-10 rounded-lg shadow-lg shadow-gray-500 ">
+      <SectionHeader heading="Transciton History" />
+      <CustomTable method="transcition" />
+    </div>
+  );
 };
