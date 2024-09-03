22374f8118b3b58f41b461be576bcca14f4e7404
rjspyk5 <rjspyk5@gmail.com>
2024-09-03 06:07:41 +0600

    unknwon

--- src/Components/Table/CustomTable.jsx
+++ src/Components/Table/CustomTable.jsx
@@ -9,7 +9,7 @@ import { styled } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "black",
+    backgroundColor: "#211128",
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
