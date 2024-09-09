f1e762137146ae56527764795054ca621df5fa19
rjspyk5 <rjspyk5@gmail.com>
2024-09-09 19:03:55 +0600

    call users data into AllUser component

--- src/Pages/Dashboard/Admin/AllUser.jsx
+++ src/Pages/Dashboard/Admin/AllUser.jsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useAxiosSequre } from "../../../Hooks/useAxiosSequre";
 
 export const AllUser = () => {
+  const axiosSequre = useAxiosSequre();
+  const { data, refetch, isLoading } = useQuery({
+    queryKey: ["allusers"],
+    queryFn: async () => {
+      const result = await axiosSequre.get("/users");
+      return result.data;
+    },
+  });
+
   return <div>AllUser</div>;
 };
