c4e28760ed6dc4589ab5664f02cfd10ccf26871e
rjspyk5 <rjspyk5@gmail.com>
2024-09-11 16:14:35 +0600

    password didnot match condition problem solved

--- src/Components/SendOrCashout/SendOrCashout.jsx
+++ src/Components/SendOrCashout/SendOrCashout.jsx
@@ -62,6 +62,7 @@ export const SendOrCashout = ({ methodparam }) => {
             })
             .then(() => navigate("/history"));
         } else {
+          setloading(false);
           Swal.fire({
             icon: "error",
             text: res?.data?.result,
