7e78038e42f0d7657a412e8e16e62ae775653e3c
rjspyk5 <rjspyk5@gmail.com>
2024-09-12 19:04:11 +0600

    unknown

---
+++ src/Pages/Dashboard/Admin/AddMoneyRequest.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAxiosSequre } from "../../../Hooks/useAxiosSequre";
+import { useAuth } from "../../../Hooks/useAuth";
+export const AddMoneyRequest = () => {
+  const axiosSequre = useAxiosSequre();
+  const { user } = useAuth();
+  const { data, refetch, isLoading } = useQuery({
+    queryKey: ["addmoneyreq"],
+    queryFn: async () => {
+      const result = await axiosSequre(
+        `/requesttoagent/${user?.number}?method=send_money`
+      );
+      return result.data;
+    },
+  });
+
+  console.log(data);
+  return <div>AddMoneyRequest</div>;
+};
