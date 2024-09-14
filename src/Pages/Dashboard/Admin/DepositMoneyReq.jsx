9b28fed3b46521d3b9cda1b2f68dbf34390c8769
rjspyk5 <rjspyk5@gmail.com>
2024-09-14 17:38:25 +0600

    basic data shown on wirthdraw req and deposit money req

--- src/Pages/Dashboard/Admin/DepositMoneyReq.jsx
+++ src/Pages/Dashboard/Admin/DepositMoneyReq.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
 import { usePendingReq } from "../../../Hooks/usePendingReq";
+import CustomTable from "../../../Components/Table/CustomTable";
+import { SectionHeader } from "../../../Components/SectionHeader/SectionHeader";
+import Swal from "sweetalert2";
+import { useAxiosSequre } from "../../../Hooks/useAxiosSequre";
 
 export const DepositMoneyReq = () => {
   const { data, refetch, isLoading } = usePendingReq("deposit_money");
-  console.log(data);
-  return <div>deposit</div>;
+  const axiossequre = useAxiosSequre();
+  const handleButton = (id, sender, rcver, action, amount) => {
+    axiossequre
+      .patch(
+        `/pendingreq/${id}?status=${action}&sender=${sender}&rcver=${rcver}&amount=${amount}`
+      )
+      .then((res) => {
+        return refetch();
+      })
+      .then(() =>
+        Swal.fire({
+          icon: "success",
+          text: `Success`,
+        })
+      );
+  };
+
+  return (
+    <div className="m-10 rounded-lg shadow-lg shadow-gray-500 ">
+      <div>
+        {isLoading && (
+          <div className=" flex flex-col justify-center items-center">
+            <Backdrop
+              sx={(theme) => ({
+                color: "#fff",
+                zIndex: theme.zIndex.drawer + 1,
+                backgroundColor: "#1311118e",
+                backdropFilter: "blur(6px)",
+              })}
+              open={true}
+            >
+              <div className="flex flex-col justify-center items-center ">
+                <CircularProgress
+                  disableShrink
+                  size={40}
+                  thickness={4}
+                  sx={{ color: "purple", marginLeft: "200px" }}
+                />
+
+                <h1 className="text-center mt-3 ml-52 font-bold text-black text-2xl">
+                  Loading........
+                </h1>
+              </div>
+            </Backdrop>
+          </div>
+        )}
+      </div>
+      <SectionHeader heading="Deposit Money Request" />
+      <CustomTable
+        handleButton={handleButton}
+        refetch={refetch}
+        loading={isLoading}
+        data={data}
+        method="Cash In"
+      />
+    </div>
+  );
 };
--- src/Pages/Dashboard/Admin/WithdrawReqeust.jsx
+++ src/Pages/Dashboard/Admin/WithdrawReqeust.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 import { usePendingReq } from "../../../Hooks/usePendingReq";
+import { SectionHeader } from "../../../Components/SectionHeader/SectionHeader";
+import CustomTable from "../../../Components/Table/CustomTable";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useAxiosSequre } from "../../../Hooks/useAxiosSequre";
+import Swal from "sweetalert2";
 
 export const WithdrawReqeust = () => {
   const { data, refetch, isLoading } = usePendingReq("withdraw_money");
+  const axiossequre = useAxiosSequre();
+  const handleButton = (id, sender, rcver, action, amount) => {
+    axiossequre
+      .patch(
+        `/pendingreq/${id}?status=${action}&sender=${sender}&rcver=${rcver}&amount=${amount}`
+      )
+      .then((res) => {
+        return refetch();
+      })
+      .then(() =>
+        Swal.fire({
+          icon: "success",
+          text: `Success`,
+        })
+      );
+  };
 
-  return <div>WithdrawReqeust</div>;
+  return (
+    <div className="m-10 rounded-lg shadow-lg shadow-gray-500 ">
+      <div>
+        {isLoading && (
+          <div className=" flex flex-col justify-center items-center">
+            <Backdrop
+              sx={(theme) => ({
+                color: "#fff",
+                zIndex: theme.zIndex.drawer + 1,
+                backgroundColor: "#1311118e",
+                backdropFilter: "blur(6px)",
+              })}
+              open={true}
+            >
+              <div className="flex flex-col justify-center items-center ">
+                <CircularProgress
+                  disableShrink
+                  size={40}
+                  thickness={4}
+                  sx={{ color: "purple", marginLeft: "200px" }}
+                />
+
+                <h1 className="text-center mt-3 ml-52 font-bold text-black text-2xl">
+                  Loading........
+                </h1>
+              </div>
+            </Backdrop>
+          </div>
+        )}
+      </div>
+      <SectionHeader heading="Deposit Money Request" />
+      <CustomTable
+        handleButton={handleButton}
+        refetch={refetch}
+        loading={isLoading}
+        data={data}
+        method="Cash In"
+      />
+    </div>
+  );
 };
