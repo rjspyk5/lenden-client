f60b87a8ce96ac62187fd86ca4d7937b74305468
rjspyk5 <rjspyk5@gmail.com>
2024-09-26 13:37:21 +0600

    change backdrop color

--- src/Pages/Dashboard/Admin/DepositMoneyReq.jsx
+++ src/Pages/Dashboard/Admin/DepositMoneyReq.jsx
@@ -27,44 +27,43 @@ export const DepositMoneyReq = () => {
 
   return (
     <div>
-      <SectionHeader heading=" Deposit Money Request" />
-      <div className="my-5 rounded-lg shadow shadow-gray-500 ">
-        <div>
-          {isLoading && (
-            <div className=" flex flex-col justify-center items-center">
-              <Backdrop
-                sx={(theme) => ({
-                  color: "#fff",
-                  zIndex: theme.zIndex.drawer + 1,
-                  backgroundColor: "#0008ff25",
-                  backdropFilter: "blur(6px)",
-                })}
-                open={true}
-              >
-                <div className="flex flex-col justify-center items-center ">
-                  <CircularProgress
-                    disableShrink
-                    size={40}
-                    thickness={4}
-                    sx={{ color: "purple", marginLeft: "200px" }}
-                  />
+      {isLoading && (
+        <div className=" flex flex-col justify-center items-center">
+          <Backdrop
+            sx={(theme) => ({
+              color: "#fff",
+              zIndex: theme.zIndex.drawer + 1,
+              backgroundColor: "#b3acac9c",
+              backdropFilter: "blur(6px)",
+            })}
+            open={true}
+          >
+            <div className="flex flex-col  justify-center items-center ">
+              <CircularProgress
+                disableShrink
+                size={40}
+                thickness={4}
+                sx={{ color: "blue", marginLeft: "200px" }}
+              />
 
-                  <h1 className="text-center mt-3 ml-52 font-bold text-[blue] text-4xl">
-                    Loading........
-                  </h1>
-                </div>
-              </Backdrop>
+              <h1 className="text-center mt-3 ml-52 font-bold text-[blue] text-4xl">
+                Loading........
+              </h1>
             </div>
-          )}
+          </Backdrop>
         </div>
-
-        <CustomTable
-          handleButton={handleButton}
-          refetch={refetch}
-          loading={isLoading}
-          data={data}
-          method="Deposit"
-        />
+      )}
+      <div>
+        <SectionHeader heading=" Deposit Money Request" />
+        <div className="my-5 rounded-lg shadow shadow-gray-500 ">
+          <CustomTable
+            handleButton={handleButton}
+            refetch={refetch}
+            loading={isLoading}
+            data={data}
+            method="Deposit"
+          />
+        </div>
       </div>
     </div>
   );
