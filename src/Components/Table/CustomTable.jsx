ec73abeccaa66f6085efc2ffc4b8b98d4c57c824
rjspyk5 <rjspyk5@gmail.com>
2024-09-26 12:30:05 +0600

    add inline css instead of importing styled cell from mui

--- src/Components/Table/CustomTable.jsx
+++ src/Components/Table/CustomTable.jsx
@@ -4,17 +4,7 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Paper, styled } from "@mui/material";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#1c24bd",
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import { Paper } from "@mui/material";
 
 const head = ["Receiver Number", "Amount", "Action"];
 
@@ -36,13 +26,18 @@ export default function CustomTable({ method, data, loading, handleButton }) {
           <TableHead>
             <TableRow>
               {head.map((el, idx) => (
-                <StyledTableCell
+                <TableCell
                   align={idx === 0 ? "left" : "center"}
                   key={idx}
-                  sx={{ padding: "12px", border: 0 }}
+                  sx={{
+                    padding: { xs: "7px", md: "12px" },
+                    border: "0px",
+                    backgroundColor: "#1c24bd",
+                    color: "white",
+                  }}
                 >
                   {el}
-                </StyledTableCell>
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
@@ -51,47 +46,52 @@ export default function CustomTable({ method, data, loading, handleButton }) {
               data.map((el) => (
                 <TableRow key={el._id}>
                   <TableCell
-                    sx={{ padding: "12px", color: "white" }}
+                    sx={{ padding: { xs: "7px", md: "12px" }, color: "white" }}
                     component="th"
                     scope="row"
                   >
                     {el?.ReciverNumber}
                   </TableCell>
                   <TableCell
-                    sx={{ padding: "12px", color: "white" }}
+                    sx={{ padding: { xs: "7px", md: "12px" }, color: "white" }}
                     align="center"
                   >
                     {el?.amount}
                   </TableCell>
-                  <TableCell sx={{ padding: "12px" }} align="center">
-                    <button
-                      onClick={() =>
-                        handleButton(
-                          el._id,
-                          el.senderNumber,
-                          el.ReciverNumber,
-                          "success",
-                          el.amount
-                        )
-                      }
-                      className="btn bg-green-500 text-white rounded-md px-2 py-1 hover:bg-green-600 hover:shadow-green-300 hover:shadow-lg"
-                    >
-                      Approve
-                    </button>
-                    <button
-                      onClick={() =>
-                        handleButton(
-                          el._id,
-                          el.senderNumber,
-                          el.ReciverNumber,
-                          "cancel",
-                          0
-                        )
-                      }
-                      className="btn bg-red-500 text-white rounded-md px-2 py-1 hover:bg-red-600 hover:shadow-red-300 hover:shadow-lg"
-                    >
-                      Cancel
-                    </button>
+                  <TableCell
+                    sx={{ padding: { xs: "7px", md: "12px" } }}
+                    align="center"
+                  >
+                    <span className="md:space-x-2 space-x-1">
+                      <button
+                        onClick={() =>
+                          handleButton(
+                            el._id,
+                            el.senderNumber,
+                            el.ReciverNumber,
+                            "success",
+                            el.amount
+                          )
+                        }
+                        className="btn bg-green-500 text-white rounded-md px-2 py-1 hover:bg-green-600 hover:shadow-green-300 hover:shadow-lg"
+                      >
+                        Approve
+                      </button>
+                      <button
+                        onClick={() =>
+                          handleButton(
+                            el._id,
+                            el.senderNumber,
+                            el.ReciverNumber,
+                            "cancel",
+                            0
+                          )
+                        }
+                        className="btn bg-red-500 text-white rounded-md px-2 py-1 hover:bg-red-600 hover:shadow-red-300 hover:shadow-lg"
+                      >
+                        Cancel
+                      </button>
+                    </span>
                   </TableCell>
                 </TableRow>
               ))}
